2385b08d5c39a93aed9d1b8a07b0ac585c7161bd
James Muranga <jmured@gmail.com>
2013-11-13 01:20:24 +0300

    Changed the format of the matrix data for easy visulization

--- js/matrix.js
+++ js/matrix.js
@@ -1,43 +1,58 @@
-var Matrix= function(){
+var Matrix = function() {
 
-    this.table =[];	
-    this.parsePoint= function(leader,latter){
-     
-		var intable = _.find(this.table, function(item){ return item.key === leader; });
-		console.log(intable);
-		if(!intable)
+    this.table = [];
+    this.searchRowForItem = function(row, itemName) {
+        var result = _.find(row,
+        function(item) {
+            return item.key === itemName
+        });
+        return result;
+    }
+    this.parsePoint = function(leader, latter) {
+        var intable = this.searchRowForItem(this.table, leader);
+        if (intable == undefined)
         {
-				this.table.push({"key":leader,value:[{"key":latter,value:1}]})
-        }else{
-            var searchResult = _.find(intable.value, function(item){ return item.key === latter });
-            if (searchResult){
-                var selectedIndex = _.indexOf(intable.value, searchResult); 
-                searchResult["value"] =  searchResult["value"]+1;
+            this.table.push({
+                "key": leader,
+                value: [{
+                    "key": latter,
+                    value: 1
+                }]
+            })
+        } else {
+            var searchResult = this.searchRowForItem(intable.value, latter);
+
+            if (searchResult != undefined) {
+                var selectedIndex = _.indexOf(intable.value, searchResult);
+                searchResult["value"] = searchResult["value"] + 1;
                 intable.value[selectedIndex] = searchResult;
-            }else{
-				this.table.push({"key":leader,value:[{"key":latter,value:1}]})
+            } else {
+                intable.value.push({
+                    "key": latter,
+                    value: 1
+                });
             }
-        }  
+        }
     };
-    this.addDataPoint= function(){
+    this.addDataPoint = function() {
         var first = arguments[0];
         var second = arguments[1];
 
-        this.parsePoint(first,second);
-        this.parsePoint(second,first);
+        this.parsePoint(first, second);
+        this.parsePoint(second, first);
 
-        if (arguments.length ==3){
+        if (arguments.length == 3) {
             var third = arguments[2];
-            this.parsePoint(third,first);
-            this.parsePoint(first,third);
-            this.parsePoint(third,second);
-            this.parsePoint(second,third);
+            this.parsePoint(third, first);
+            this.parsePoint(first, third);
+            this.parsePoint(third, second);
+            this.parsePoint(second, third);
 
         }
     };
-    this.parseArray= function(args){
-        this.addDataPoint.apply(this,args);
+    this.parseArray = function(args) {
+        this.addDataPoint.apply(this, args);
     };
-	 
-	
+
+
 };
--- js/tests/spec/MatrixSpec.js
+++ js/tests/spec/MatrixSpec.js
@@ -1,149 +1,247 @@
-describe("Matrix", function() {
-	var matrix;
-	beforeEach(function() {
-		matrix = new Matrix();
+describe("Matrix",
+function() {
+    var matrix;
+    beforeEach(function() {
+        matrix = new Matrix();
 
-	});
+    });
 
-	it("should be able to add a single data point", function() {
-		matrix.addDataPoint("MJ", "FELIX");
-		expect(matrix.table).toEqual(
-			[{
-				"key": "MJ",
-				"value": [{
-					"key": "FELIX",
-					"value": 1
-				}]
-			}, {
-				"key": "FELIX",
-				"value": [{
-					"key": "MJ",
-					"value": 1
-				}]
-			}]);
-	});
-	it("should be able to add two data points", function() {
-		matrix.addDataPoint("MJ", "FELIX");
-		matrix.addDataPoint("MJ", "FELIX");
-		expect(matrix.table).toEqual(
-			[{
-				"key": "MJ",
-				"value": [{
-					"key": "FELIX",
-					"value": 2
-				}]
-			}, {
-				"key": "FELIX",
-				"value": [{
-					"key": "MJ",
-					"value": 2
-				}]
-			}]);
+    describe("searchRowForItem",
+    function() {
+        it("should find item in array",
+        function() {
+            var result = matrix.searchRowForItem([{
+                "key": "MJ"
+            }], "MJ");
+            expect(result).toEqual({
+                "key": "MJ"
+            });
+        });
 
-	});
-	it("should be able to three data points", function() {
-		matrix.addDataPoint("MJ", "FELIX");
-		matrix.addDataPoint("MJ", "FELIX");
-		matrix.addDataPoint("MJ", "FELIX");
-		expect(matrix.table).toEqual(
-			[{
-				"key": "MJ",
-				"value": [{
-					"key": "FELIX",
-					"value": 3
-				}]
-			}, {
-				"key": "FELIX",
-				"value": [{
-					"key": "MJ",
-					"value": 3
-				}]
-			}]);
+        it("should return undefined for an item in not in the  array",
+        function() {
+            var result = matrix.searchRowForItem([{
+                "key": "JK"
+            }], "MJ");
+            expect(result).toEqual(undefined);
+        });
 
-	});
-	it("should be able to two different data points", function() {
-		matrix.addDataPoint("MJ", "FELIX");
-		matrix.addDataPoint("MJ", "JS");
-		expect(matrix.table).toEqual(
+    });
 
-			[{
-				"key": "MJ",
-				"value": [{
-					"key": "FELIX",
-					"value": 1
-				}, {
-					"key": "JS",
-					"value": 1
-				}]
-			}, {
-				"key": "FELIX",
-				"value": [{
-					"key": "MJ",
-					"value": 1
-				}]
-			}, {
-				"key": "JS",
-				"value": [{
-					"key": "MJ",
-					"value": 1
-				}]
-			}]);
+    describe("addDataPoint",
+    function() {
+        it("should be able to add a single data point",
+        function() {
+            matrix.addDataPoint("MJ", "FELIX");
+            expect(matrix.table).toEqual(
+            [{
+                "key": "MJ",
+                "value": [{
+                    "key": "FELIX",
+                    "value": 1
+                }]
+            },
+            {
+                "key": "FELIX",
+                "value": [{
+                    "key": "MJ",
+                    "value": 1
+                }]
+            }]);
+        });
+        it("should be able to add two data points",
+        function() {
+            matrix.addDataPoint("MJ", "FELIX");
+            matrix.addDataPoint("MJ", "FELIX");
+            expect(matrix.table).toEqual(
+            [{
+                "key": "MJ",
+                "value": [{
+                    "key": "FELIX",
+                    "value": 2
+                }]
+            },
+            {
+                "key": "FELIX",
+                "value": [{
+                    "key": "MJ",
+                    "value": 2
+                }]
+            }]);
 
-	});
-	it("should be able to enter  three data points", function() {
-		matrix.addDataPoint("MJ", "FELIX", "JS");
-		expect(matrix.table).toEqual({
-			"MJ": [{
-				"FELIX": 1
-			}, {
-				"JS": 1
-			}],
-			"FELIX": [{
-				"MJ": 1
-			}, {
-				"JS": 1
-			}],
-			"JS": [{
-				"MJ": 1
-			}, {
-				"FELIX": 1
-			}]
-		});
+        });
+        it("should be able to three data points",
+        function() {
+            matrix.addDataPoint("MJ", "FELIX");
+            matrix.addDataPoint("MJ", "FELIX");
+            matrix.addDataPoint("MJ", "FELIX");
+            expect(matrix.table).toEqual(
+            [{
+                "key": "MJ",
+                "value": [{
+                    "key": "FELIX",
+                    "value": 3
+                }]
+            },
+            {
+                "key": "FELIX",
+                "value": [{
+                    "key": "MJ",
+                    "value": 3
+                }]
+            }]);
 
-	});
+        });
+        it("should be able to two different data points",
+        function() {
+            matrix.addDataPoint("MJ", "FELIX");
+            matrix.addDataPoint("MJ", "JS");
+            expect(matrix.table).toEqual(
+
+            [{
+                "key": "MJ",
+                "value": [{
+                    "key": "FELIX",
+                    "value": 1
+                },
+                {
+                    "key": "JS",
+                    "value": 1
+                }]
+            },
+            {
+                "key": "FELIX",
+                "value": [{
+                    "key": "MJ",
+                    "value": 1
+                }]
+            },
+            {
+                "key": "JS",
+                "value": [{
+                    "key": "MJ",
+                    "value": 1
+                }]
+            }]);
+
+        });
+        it("should be able to enter  three data points",
+        function() {
+            matrix.addDataPoint("MJ", "FELIX", "JS");
+            expect(matrix.table).toEqual(
+            [{
+                "key": "MJ",
+                "value": [
+                {
+                    "key": "FELIX",
+                    "value": 1
+                },
+                {
+                    "key": "JS",
+                    "value": 1
+                }
+                ]
+            },
+            {
+                "key": "FELIX",
+                "value": [
+                {
+                    "key": "MJ",
+                    "value": 1
+                },
+                {
+                    "key": "JS",
+                    "value": 1
+                }
+                ]
+            },
+
+            {
+                "key": "JS",
+                "value": [
+                {
+                    "key": "MJ",
+                    "value": 1
+                },
+                {
+                    "key": "FELIX",
+                    "value": 1
+                }
+
+                ]
+            }
+            ]
+            );
+        });
+
+        it("should be able to two sets of  data points",
+        function() {
+            matrix.addDataPoint("MJ", "FELIX");
+            matrix.addDataPoint("FE", "JS");
+            expect(matrix.table).toEqual(
+            [{
+                "key": "MJ",
+                "value": [{
+                    "key": "FELIX",
+                    "value": 1
+                }]
+            },
+            {
+                "key": "FELIX",
+                "value": [{
+                    "key": "MJ",
+                    "value": 1
+                }]
+            },
+            {
+                "key": "FE",
+                "value": [{
+                    "key": "JS",
+                    "value": 1
+                }]
+            },
+            {
+                "key": "JS",
+                "value": [{
+                    "key": "FE",
+                    "value": 1
+                }]
+            }
+            ]
+            );
 
 
-	it("should be able to two sets of  data points", function() {
-		matrix.addDataPoint("MJ", "FELIX");
-		matrix.addDataPoint("FE", "JS");
-		expect(matrix.table).toEqual({
-			"MJ": [{
-				"FELIX": 1
-			}],
-			"FELIX": [{
-				"MJ": 1
-			}],
-			"FE": [{
-				"JS": 1
-			}],
-			"JS": [{
-				"FE": 1
-			}]
-		});
+        });
 
-	});
-	it("should parse array input ", function() {
-		matrix.parseArray(["MJ", "FELIX"]);
-		expect(matrix.table).toEqual({
-			"MJ": [{
-				"FELIX": 1
-			}],
-			"FELIX": [{
-				"MJ": 1
-			}]
-		});
+    });
 
-	});
+
+    describe("parseArray",
+    function() {
+
+        it("should parse array input ",
+        function() {
+            matrix.parseArray(["MJ", "FELIX"]);
+            expect(matrix.table).toEqual(
+            [{
+                "key": "MJ",
+                "value": [{
+                    "key": "FELIX",
+                    "value": 1
+                }]
+            },
+            {
+                "key": "FELIX",
+                "value": [{
+                    "key": "MJ",
+                    "value": 1
+                }]
+            }]);
+
+        });
+    });
+
+
 
 
 
